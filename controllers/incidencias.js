edf72b59912bee5a9cf1f64520bf4b95a2767e76
Jose Selma Hoyo <jlselma25@gmail.com>
2026-01-30 18:38:38 +0100

    orden prioridad

--- controllers/incidencias.js
+++ controllers/incidencias.js
@@ -129,7 +129,7 @@ GuardarIncidencia = async(req, res = response ) => {
 
        
         if (individual == 0){
-            sql  = "SELECT Fecha, id, Motivo Titulo, Tienda, Estado, Observaciones, C.Nombre, Prioridad FROM IncidenciasManteTiendas T JOIN Clientes C ON T.Tienda = C.Numero WHERE Fecha >='" + fechaDesdeFormateada + "' AND Fecha <='" + fechaHastaFormateada + "'" + filtroEstado + filtroTiendas + filtroPrioridad + " ORDER BY Prioridad DESC, Fecha "
+            sql  = "SELECT Fecha, id, Motivo Titulo, Tienda, Estado, Observaciones, C.Nombre, Prioridad FROM IncidenciasManteTiendas T JOIN Clientes C ON T.Tienda = C.Numero WHERE Fecha >='" + fechaDesdeFormateada + "' AND Fecha <='" + fechaHastaFormateada + "'" + filtroEstado + filtroTiendas + filtroPrioridad + " ORDER BY Prioridad , Fecha "
         }else{            
             sql = "SELECT Fecha, id, Motivo Titulo, Tienda, Estado, '' Nombre, Observaciones, Prioridad FROM IncidenciasManteTiendas WHERE id =" + id.ToString(); 
         }       
